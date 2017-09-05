bbcc4107ae64a78629f4c2a7262f233d46577d97
hlaaf <hlaaftana@users.noreply.github.com>
2017-09-06 00:07:46 +0300

    Add sslcontext argument to URI proc parameters
    Missed this last time

--- websocket/client.nim
+++ websocket/client.nim
@@ -113,7 +113,8 @@ proc newAsyncWebsocket*(host: string, port: Port, path: string, ssl = false,
 
 proc newAsyncWebsocket*(uri: Uri, additionalHeaders: seq[(string, string)] = @[], 
     protocols: seq[string] = @[],
-    userAgent: string = WebsocketUserAgent
+    userAgent: string = WebsocketUserAgent,
+    ctx: SslContext = newContext(protTLSv1)
    ): Future[AsyncWebSocket] {.async.} =
   var ssl: bool
   if uri.scheme == "ws":
@@ -124,15 +125,16 @@ proc newAsyncWebsocket*(uri: Uri, additionalHeaders: seq[(string, string)] = @[]
     raise newException(ProtocolError, "uri scheme has to be 'ws' for plaintext or 'wss' for websocket over ssl.")
 
   let port = Port(uri.port.parseInt())
-  return await newAsyncWebsocket(uri.hostname, port , uri.path, ssl,
-    additionalHeaders, protocols, userAgent)
+  return await newAsyncWebsocket(uri.hostname, port, uri.path, ssl,
+    additionalHeaders, protocols, userAgent, ctx)
   
 proc newAsyncWebsocket*(uri: string, additionalHeaders: seq[(string, string)] = @[], 
     protocols: seq[string] = @[],
-    userAgent: string = WebsocketUserAgent
+    userAgent: string = WebsocketUserAgent,
+    ctx: SslContext = newContext(protTLSv1)
    ): Future[AsyncWebSocket] {.async.} =
   let uriBuf = parseUri(uri)
-  return await newAsyncWebsocket(uriBuf, additionalHeaders, protocols, userAgent)
+  return await newAsyncWebsocket(uriBuf, additionalHeaders, protocols, userAgent, ctx)
 
 # proc sendFrameData(ws: AsyncWebSocket, data: string): Future[void] {.async.} =
 #   await ws.sock.send(data)
